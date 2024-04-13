1f1c07ab0fb8ba8d72fd8a94d02752a595e7e1e1
jabes.rivas <jabesalberto02@gmail.com>
2024-04-13 00:20:49 -0400

    Nuevo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='anko',
+   version='1.0',
+   description='Anko Genshin API',
+   author='jabes',
+   author_email='jabes@dangos.dev',
+   packages=['C:\\Users\\jrivas\\source\\python\\genshin-api'],  #same as name
+   install_requires=['flask', 'flask_cors'], #external packages as dependencies
+)
