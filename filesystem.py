b57fdfff2ae660f2868149559dc051c081b1ce2e
jabes.rivas <jabesalberto02@gmail.com>
2024-04-13 19:30:19 -0400

    Carga masiva de archivos de imagen

--- filesystem.py
+++ filesystem.py
@@ -1,39 +1,70 @@
 import os
 import json
+from flask import send_file
+
 
 DATA_PATH = f'./static/data'
-IMAGE_PATH = f'./static/image'
+IMAGE_PATH = f'./static/images'
 DEFAULT_LANG = 'en'
 
+
+
 class Filesystem:
-    
+
     def getEndpoints(self):
-        return [f for f in os.listdir(self.__dataDirectory()) if os.path.isdir(self.__dataDirectory(f))]
-    
+        endpoints = [f for f in os.listdir(self.__dataDirectory()) if os.path.isdir(self.__dataDirectory(f))]
+        return endpoints
+
     def getEndpointEntities(self, endpoint):
         is_endpoint = os.path.isdir(self.__dataDirectory(endpoint))
-        
+
         # 404, not found
         if not is_endpoint:
-            return {'error' : f'Endpoint {endpoint} not found. Go to /endpoints to see a list of available endpoints'}, 404
-             
+            return {'error': f'Endpoint {endpoint} not found. Go to /endpoints to see a list of available endpoints'}, 404
+
         return [f for f in os.listdir(self.__dataDirectory(endpoint)) if os.path.isdir(self.__dataDirectory(endpoint, f))]
 
-    def getEntity(self, endpoint: str, entity: str, lang:str = DEFAULT_LANG):
-        entityPath = os.path.join(self.__dataDirectory(endpoint, entity), f'{lang}.json' )
+    def getEntity(self, endpoint: str, entity: str, lang: str = DEFAULT_LANG):
+        entityPath = os.path.join(self.__dataDirectory(
+            endpoint, entity), f'{lang}.json')
         exists = os.path.isfile(entityPath)
 
         if not exists:
-            return {'error' : f'Entity /{endpoint}/{entity} not found.'}, 404
-        
-        with open(entityPath) as user_file:
-            parsed_json = json.load(user_file)
+            return {'error': f'Entity /{endpoint}/{entity} not found.'}, 404
+
+        try:
+            with open(entityPath) as user_file:
+                parsed_json = json.load(user_file)
+        except:
+            return {'error': f'Error in JSON formatting of entity {endpoint}/${entity} for language {lang}'}, 500
 
         return parsed_json
 
+    def getImages(self, endpoint: str, entity: str):
+        is_endpoint = os.path.isdir(self.__dataDirectory(endpoint))
+        is_entity = os.path.isdir(self.__dataDirectory(endpoint, entity))
+
+        # 404, not found
+        if not is_endpoint:
+            return {'error': f'Endpoint {endpoint} not found. Go to /endpoints to see a list of available endpoints'}, 404
+
+        # 404, not found
+        if not is_entity:
+            return {'error': f'Entity {entity} not found. Go to /{endpoint} to see a list of available entities'}, 404
+
+        return [f for f in os.listdir(self.__imageDirectory(endpoint, entity)) if os.path.isfile(self.__imageDirectory(endpoint, entity, f), )]
+
+    def getImage(self, endpoint: str, entity: str, image: str, format: str = 'webp'):
+        imagePath = self.__imageDirectory(endpoint, entity, image)
+
+        is_image = os.path.isfile(imagePath)
+        if not is_image:
+            return {'error': f'Image {entity}/{image} not found. Go to /{entity}/images to see a list of available images'}, 404
+
+        return send_file(imagePath, mimetype=f'image/{format}'), 200
 
     def __dataDirectory(self, endpoint: str = '', entity: str = '') -> str:
         return os.path.join(DATA_PATH.lower(), endpoint.lower(), entity.lower())
-    
-    def __imageDirectory(self, endpoint: str = '') -> str:
-        return os.path.join(IMAGE_PATH, endpoint)
+
+    def __imageDirectory(self, endpoint: str = '', entity: str = '', image: str = '') -> str:
+        return os.path.join(IMAGE_PATH.lower(), endpoint.lower(), entity.lower(), image.lower())
--- server.py
+++ server.py
@@ -1,25 +1,64 @@
 from flask import Flask, request
-from flask_cors import CORS
-import os
+from flask_caching import Cache
 from filesystem import Filesystem
 
+config = {
+    "DEBUG": True,
+    "CACHE_TYPE": "SimpleCache",
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
+
+cache = Cache(config=config)
+
 app = Flask(__name__)
-CORS(app)
+app.config.from_mapping(config)
+cache = Cache(app)
 
 fsy = Filesystem()
 
+
 @app.route('/')
 @app.route('/endpoints')
+@cache.cached()
 def entry_point():
     return fsy.getEndpoints()
 
+
 @app.route('/<endpoint>')
+@cache.cached()
 def endpoints(endpoint):
     return fsy.getEndpointEntities(endpoint)
 
+
+@app.route('/<endpoint>/all')
+@cache.cached()
+def entities_all(endpoint):
+    availableEntities = fsy.getEndpointEntities(endpoint)
+    entities = []
+    for entity in availableEntities:
+        entities.append(fsy.getEntity(endpoint, entity))
+
+    return entities
+
+
 @app.route('/<endpoint>/<entity>')
+@cache.cached()
 def entity(endpoint, entity):
     return fsy.getEntity(endpoint, entity)
+
+
+@app.route('/<endpoint>/<entity>/images')
+@cache.cached()
+def images(endpoint, entity):
+    return fsy.getImages(endpoint, entity)
+
+
+@app.route('/<endpoint>/<entity>/<image>')
+@app.route('/<endpoint>/<entity>/<image>/<format>')
+@cache.cached()
+def image(endpoint, entity, image, format='webp'):
+    return fsy.getImage(endpoint, entity, image, format)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8001)
